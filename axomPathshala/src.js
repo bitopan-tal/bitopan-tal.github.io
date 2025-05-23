87fa01b2832da5e9cbce903d3f2abfe04fafe1ce
BITOPAN TALUKDAR <bitopan.tal@gmail.com>
2025-05-24 01:39:55 +0530

    Update src.js

--- axomPathshala/src.js
+++ axomPathshala/src.js
@@ -2,7 +2,7 @@ var viewMode = getCookie("view-mode");
 if(viewMode == "desktop"){
     viewport.setAttribute('content', 'width=1024');
 }else if (viewMode == "mobile"){
-    viewport.setAttribute('content', 'width=device-width,initial-scale=0.1,maximum-scale=1.0,user-scalable=no');
+    viewport.setAttribute('content', 'width=device-width,initial-scale=0.1,maximum-scale=0.1,user-scalable=no');
 }
 
 document.addEventListener('DOMContentLoaded', function () {
