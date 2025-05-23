1c547f001c5be29a722c78e8b16d48eeb252217b
BITOPAN TALUKDAR <bitopan.tal@gmail.com>
2025-05-23 20:08:07 +0530

    fixing aspect ratio

--- axomPathshala/src.js
+++ axomPathshala/src.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('video1').addEventListener('click', function () {
         // alert("Hello, world!");
-        var videoId = '7AgEjgUtho4'; // Replace with your actual video ID
+        var videoId = '8qRGg4L3D6toHnTN&amp;list=PLVLoWQFkZbhU9-R4Ln3nJ5WhKIe5tmgye'; // Replace with your actual video ID
         var iframe = document.createElement('iframe');
-        iframe.setAttribute('src', 'https://www.youtube.com/embed/' + videoId + '?autoplay=1&mute=1&rel=0');
+        iframe.setAttribute('src', 'https://www.youtube-nocookie.com/embed/videoseries?si=' + videoId);
         iframe.setAttribute('frameborder', '0');
         iframe.setAttribute('allowfullscreen', '');
         iframe.style.width = '100%';
+        // iframe.style.aspect-ratio= '16/9';
         iframe.style.height = '100%';
 
         var container = document.getElementById('player');
